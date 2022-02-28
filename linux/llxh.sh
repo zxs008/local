4e55d2428ddb9b4dad2dd514300e6381dae07d22
zxs008 <78520941+zxs008@users.noreply.github.com>
2022-02-28 16:40:34 +0800

    Update llxh.sh

--- linux/llxh.sh
+++ linux/llxh.sh
@@ -4,6 +4,7 @@
 #Blog https://ty.al
 
 url=""
+numbe=""
 
 function MediaUnlockTest() {
   echo -e "${url}"
@@ -11,8 +12,8 @@ function MediaUnlockTest() {
   do
     timeData=$(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")
     echo -e "${timeData}"
-    wget ${url} -O ll.test
-    rm -rf ll.test
+    wget ${url} -O ll${num1}.test
+    rm -rf ll${num1}.test
   done
 }
 
@@ -39,73 +40,89 @@ show_menu() {
     ${green}0.${plain}  退出脚本
     "
     echo && read -ep "请输入选择 [0-13]: " num
-
+    echo && read -ep "数字: " num1
     case "${num}" in
     0)
         exit 0
         ;;
     1)
         url="https://hnd-jp-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     2)
         url="https://sgp-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     3)
         url="https://sel-kor-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     4)
         url="https://lon-gb-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     5)
         url="https://fra-de-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     6)
         url="https://par-fr-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     7)
         url="https://ams-nl-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     8)
         url="https://fl-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     9)
         url="https://ga-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     10)
         url="https://il-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     11)
         url="https://sjo-ca-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     12)
         url="https://tx-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     13)
         url="https://lax-ca-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     14)
         url="https://nj-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     15)
         url="https://wa-us-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     16)
         url="https://syd-au-ping.vultr.com/vultr.com.1000MB.bin";
+        numbe=${num1}
         MediaUnlockTest
         ;;
     *)
