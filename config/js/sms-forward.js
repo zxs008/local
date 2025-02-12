34d12b7e2c644f726eac6575422a6cc24050913e
zxs008 <78520941+zxs008@users.noreply.github.com>
2025-02-12 17:55:42 +0900

    Update sms-forward.js

--- config/js/sms-forward.js
+++ config/js/sms-forward.js
@@ -259,7 +259,8 @@ async function notify(title, subtitle, body, { copy, KEY_PUSHDEER, KEY_BARK }) {
       try {
         const url = bark
           .replace('[推送标题]', encodeURIComponent(title))
-          .replace('[推送内容]', encodeURIComponent(`${body}`))
+          .replace('[推送内容]', encodeURIComponent(`${subtitle}\n${body}`))
+          .replace('[推送内容去副标]', encodeURIComponent(`${body}`))
           .replace('[复制内容]', encodeURIComponent(copy))
         $.log(`开始 bark 请求: ${url}`)
         const res = await $.http.get({ url })
