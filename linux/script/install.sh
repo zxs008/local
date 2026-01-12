52bd1b4c39000b8d4342e8d36321b62f99a5dac5
zxs008 <78520941+zxs008@users.noreply.github.com>
2026-01-12 18:12:54 +0900

    Update install.sh

--- linux/script/install.sh
+++ linux/script/install.sh
@@ -203,9 +203,9 @@ download_agent(){
     fi
 
     echo -e "正在下载监控端"
-    wget -O nezha-agent_linux_${os_arch}.tar.gz https://${Github_Cn}${GITHUB_URL}/zxs008/nezha_bak/releases/download/${version}/nezha-agent_linux_${os_arch}.tar.gz >/dev/null 2>&1
+    wget -O nezha-agent_linux_${os_arch}.tar.gz ${Github_Cn}https://${GITHUB_URL}/zxs008/nezha_bak/releases/download/${version}/nezha-agent_linux_${os_arch}.tar.gz >/dev/null 2>&1
     if [[ $? != 0 ]]; then
-        wget -O nezha-agent_linux_${os_arch}.zip https://${Github_Cn}${GITHUB_URL}/zxs008/nezha_bak/releases/download/${version}/nezha-agent_linux_${os_arch}.zip >/dev/null 2>&1
+        wget -O nezha-agent_linux_${os_arch}.zip ${Github_Cn}https://${GITHUB_URL}/zxs008/nezha_bak/releases/download/${version}/nezha-agent_linux_${os_arch}.zip >/dev/null 2>&1
         if [[ $? != 0 ]]; then
            echo -e "${red}Release 下载失败，请检查本机能否连接 ${Github_Cn}${GITHUB_URL}${plain}"
            return 0
